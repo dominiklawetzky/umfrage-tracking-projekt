8a3fae5ad960d0a0a904b1b7e77c4e19a01a7c8c
Dominik Lawetzky <dominik@lawetzky.de>
2021-07-13 11:18:22 +0200

    Weitere Institute ergänzen

--- Sonntagsfrage.R
+++ Sonntagsfrage.R
@@ -1,9 +1,9 @@
 ##### Preamble -----
 
-#rm(list = ls())
+rm(list = ls())
 
 # Working Directory
-#setwd("/Users/dominiklawetzky/Documents/GitHub/sonntagsfrage")
+setwd("/Users/dominiklawetzky/Documents/GitHub/sonntagsfrage")
 
 ## PACKAGE NAMEN
 packages <- c("ggplot2", "readxl", "dplyr", "multcomp", "tidyr", "knitr", "car", "psych", "tidyverse", "lmtest", "ggpubr", "ggstatsplot", "jsonlite", "pander", "abind", "RColorBrewer", "rococo", "shiny", "gvlma", "emmeans", "ez")
@@ -22,9 +22,7 @@ if (any(installed_packages == FALSE)) {
 invisible(lapply(packages, library, character.only = TRUE))
 
 
-
-
-##### Daten laden -----
+##### Infratest laden -----
 
 infratest <- data.frame(
   stringsAsFactors = FALSE,
@@ -697,18 +695,70 @@ infratest <- data.frame(
              )
 
 
-
 infratest$AfD <- round(as.numeric(infratest$AfD), 0)
 infratest$FDP <- round(as.numeric(infratest$FDP), 0)
 infratest$Piraten <- round(as.numeric(infratest$Piraten), 0)
 infratest$Datum <- format(as.Date(infratest$Datum, format="%d.%m.%y"), "%d.%m.%y")
 
 infratest_long <- infratest %>%
-                      pivot_longer(!Datum, names_to = "Partei", values_to = "Prozent")
+                      pivot_longer(!Datum, names_to = "Partei", values_to = "Prozent") # In Longformat umwandeln
 
 infratest_long$Partei <- as.factor(infratest_long$Partei)
 infratest_long$Datum <- format(as.Date(infratest_long$Datum, format="%d.%m.%y"), "%d.%m.%y")
 
+infratest_long$Institut <- c("Infratest")
+
+
+##### Allensbach laden -----
+
+allensbach <- read.csv("allensbach.csv", header = TRUE, sep = c(";", " - "), dec = ",")
+
+allensbach$Datum <- substr(allensbach$Zeitraum, 9, nchar(allensbach$Zeitraum)) # Nur Endzeitpunkt der Erhebung als Datum
+
+allensbach$Datum <- format(as.Date(allensbach$Datum, format="%d.%m.%Y"), "%d.%m.%y") # Datumsformat
+
+names(allensbach)[2] <- "Union"
+names(allensbach)[5] <- "Grüne"
+names(allensbach)[6] <- "Linke*"
+names(allensbach)[7] <- "AfD"
+names(allensbach)[8] <- "Andere"
+
+allensbach <- subset(allensbach, select = -c(Zeitraum)) # Variable Zeitraum löschen
+
+
+allensbach_long <- allensbach %>%
+                        pivot_longer(!Datum, names_to = "Partei", values_to = "Prozent") # In Longformat umwandeln
+
+allensbach_long$Institut <- c("Allensbach")
+
+
+##### Forschungsgruppe Wahlen -----
+
+forschungsgruppe <- read.csv("forschungsgruppe.csv", header = TRUE, sep = c(";"), dec = ".", na.strings = "NA")
+
+forschungsgruppe <- subset(forschungsgruppe, select = -c(X, n, Zeitraum)) # Überflüssige Variablen löschen
+
+names(forschungsgruppe)[6] <- "Linke*" 
+
+forschungsgruppe$Piraten <- as.numeric(forschungsgruppe$Piraten)
+forschungsgruppe$Union <- as.numeric(forschungsgruppe$Union)
+forschungsgruppe$FDP <- as.numeric(forschungsgruppe$FDP)
+forschungsgruppe$AfD <- as.numeric(forschungsgruppe$AfD)
+
+forschungsgruppe$Datum <- format(as.Date(forschungsgruppe$Datum, format="%d.%m.%Y"), "%d.%m.%y") # Datumsformat
+
+forschungsgruppe_long <- forschungsgruppe %>%
+                             pivot_longer(!Datum, names_to = "Partei", values_to = "Prozent") # In Longformat umwandeln
+
+forschungsgruppe_long$Institut <- c("Forschungsgruppe Wahlen")
+
+forschungsgruppe_long$Prozent <- forschungsgruppe_long$Prozent*100
+
+##### Datensätze aggregieren -----
+
+dataset <- rbind(allensbach_long, infratest_long, forschungsgruppe_long)
+
+dataset$Institut <- as.factor(dataset$Institut)
 
 ##### Deskriptivstatistik -----
 
@@ -716,25 +766,34 @@ infratest_long$Datum <- format(as.Date(infratest_long$Datum, format="%d.%m.%y"),
 colors <- c("#019ee3", "#7c7c7c", "#fbeb04", "#1ca42c", "#bd3076", "#f39200", "#e2001a", "#000000")
 colors_alt <- c("#019ee3", "#7c7c7c", "#fbeb04", "#1ca42c", "#bd3076", "#e2001a", "#000000")
 
-## PLOT 1
+## PLOT 1 (10 Jahre / alle Institute / alle Parteien)
 
 min <- as.Date("07.01.10", format = "%d.%m.%y")
 max <- as.Date("01.07.21", format = "%d.%m.%y")
 
-plot1 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y"), y = Prozent, fill = as.factor(Partei))) +
-                  geom_bar(stat = "identity") +
+plot1 <- ggplot(data = dataset, aes(x = as.Date(Datum, format = "%d.%m.%y"), y = Prozent, color = as.factor(Partei), linetype = as.factor(Institut), shape = as.factor(Institut))) +
+                  geom_point(stat = "identity", size = .8) +
+                  geom_line() +
+                  #geom_smooth(method = "loess", se = TRUE, span = 2) +
                   labs(title = "Zustimmungswerte der großen politischen Parteien", 
-                       subtitle = "seit Januar 2010",
-                       color = "Legende",
+                       subtitle = "seit Januar 2010 über verschiedene Umfrage-Institute",
+                       color = "Parteien",
+                       linetype = "Institute",
+                       shape = "Institute",
                        x = "Datum",
-                       caption = "Daten: Infratest Dimap / Aufbereitung: Dominik Lawetzky") +
-                  scale_fill_manual(name = "Legende", values = colors) +
+                       caption = "github.com/dominiklawetzky/sonntagsfrage") +
+                  scale_color_manual(name = "Legende", values = colors) +
                   theme_light() +
-                  theme(axis.text.x=element_text(size=rel(0.5), angle=90)) +
+                  theme(axis.text.x=element_text(size=rel(.75), angle=90)) +
+                  theme(plot.title = element_text(size = 18, face = "bold")) +
                   theme(legend.position="bottom") +
-                  scale_x_date(breaks = "1 month", limits = c(min, max))
+                  scale_x_date(breaks = "6 month", limits = c(min, max))
 
-ggsave(file="plot1.jpg", plot=plot1, width=15, height=4)
+
+ggsave(file="plot1.jpg", plot=plot1, width=15, height=8)
+
+
+
 
 ## PLOT 2
 
@@ -755,6 +814,8 @@ plot2 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y
                 scale_x_date(breaks = "1 month", limits = c(min, max))
 
 ggsave(file="plot2.jpg", plot=plot2, width=8, height=6)
+
+
 
 
 ## PLOT 3
@@ -780,6 +841,8 @@ ggsave(file="plot3.jpg", plot=plot3, width=8, height=6)
 
 
 
+
+
 ## PLOT 4
 
 min <- as.Date("09.01.20", "%d.%m.%y")
@@ -788,7 +851,7 @@ max <- as.Date("01.07.21", "%d.%m.%y")
 plot4 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y"), y = Prozent, color = as.factor(Partei))) +
                   geom_line(stat = "identity", size = 1) +
                   geom_point(size = 1) +
-                  geom_smooth(aes(x = as.Date(Datum, format = "%d.%m.%y"), y = Prozent, color = as.factor(Partei)), method = "loess", span = .5, size = 1, linetype = "dashed") +
+                  geom_smooth(aes(x = as.Date(Datum, format = "%d.%m.%y"), y = Prozent, color = as.factor(Partei)), method = "loess", span = .5, size = 1, linetype = "dashed") 
                   labs(title = "Zustimmungswerte der großen politischen Parteien", 
                        subtitle = "seit Januar 2020 / mit LOESS-Glättung",
                        color = "Legende",
@@ -801,6 +864,9 @@ plot4 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y
                   scale_x_date(breaks = "1 month", limits = c(min, max))
 
 ggsave(file="plot4.jpg", plot=plot4, width=8, height=6)
+
+
+
 
 
 ## PLOT 5
@@ -825,6 +891,8 @@ ggsave(file="plot5.jpg", plot=plot5, width=10, height=6)
 
 
 
+
+
 ## PLOT 6
 
 min <- as.Date("07.01.10", "%d.%m.%y")
@@ -845,6 +913,9 @@ plot6 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y
   scale_x_date(breaks = "1 month", limits = c(min, max))
 
 ggsave(file="plot6.jpg", plot=plot6, width=10, height=6)
+
+
+
 
 
 ## PLOT 7
@@ -871,6 +942,8 @@ ggsave(file="plot7.jpg", plot=plot7, width=10, height=6)
 
 
 
+
+
 ## PLOT 7
 
 min <- as.Date("04.01.18", "%d.%m.%y")
@@ -878,7 +951,6 @@ max <- as.Date("01.07.21", "%d.%m.%y")
 
 plot7 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y"), y = Prozent, color = as.factor(Partei))) +
   geom_line(stat = "identity", size = 1) +
-  stat_growthcurve() +
   labs(title = "Zustimmungswerte der großen politischen Parteien", 
        subtitle = "seit Januar 2018 / mit LOESS-Glättung",
        color = "Legende",
@@ -892,3 +964,5 @@ plot7 <- ggplot(data = infratest_long, aes(x = as.Date(Datum, format = "%d.%m.%y
 
 
 ggsave(file="plot7.jpg", plot=plot7, width=10, height=6)
+
+
