207312703b8d55a5d5fea2e8529f4bb79ab08ac2
Dominik Lawetzky <dominik@lawetzky.de>
2021-07-25 11:48:08 +0200

    Plotly-Umsetzung hinzugefügt

--- Shiny.R
+++ Shiny.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(ggthemes)
 library(dplyr)
 library(scales)
+library(plotly)
 
 
 ##### UI ------
@@ -25,15 +26,15 @@ ui <- fluidPage(theme = shinythemes::shinytheme("simplex"),
                     #             selected = "Travel"),
                     
                     checkboxGroupInput(inputId = "institut", label = strong("Umfrage-Institut"),
-                                       selected = unique(dataset$Institut),
-                                       choices = unique(dataset$Institut)),
+                                       selected = c("Allensbach", "Forschungsgruppe Wahlen", "Infratest"),
+                                       choices = c("Allensbach", "Forschungsgruppe Wahlen", "Infratest")),
                     
                     # ZEITINTERVALL AUSWÄHLEN
                     
                     dateRangeInput("date", strong("Betrachtungszeitraum"), start = "2010-01-01", end = "2021-07-20",
                                    min = "2010-01-01", max = "2021-07-20",
-                                   language = "de", separator = "bis"),
-                    
+                                   format = "dd.mm.yyyy", language = "de", separator = "bis"),
+
                     
                     # LOESS-GLÄTTUNG JA/NEIN
                     checkboxInput(inputId = "smoother", label = strong("LOESS-Glättung aktivieren"), value = FALSE),
@@ -58,8 +59,8 @@ ui <- fluidPage(theme = shinythemes::shinytheme("simplex"),
                                 # TAB 1
                                 tabPanel("Übersicht", 
                                          
-                                         plotOutput("plot1",
-                                                    width = "100%"),
+                                         plotlyOutput("plot1",
+                                                      height = "auto", width = "auto"),
                                          downloadButton("plot1_download_pdf", label = "Als PDF herunterladen")
                                          ),
                                 
@@ -99,188 +100,51 @@ ui <- fluidPage(theme = shinythemes::shinytheme("simplex"),
 ##### Server Logic ------
 server <- function(input, output, session) {
   
-  output$plot1 <- renderPlot({
-    
-    # ZEITINTERVALL FESTLEGEN
-    
-    min <- as.Date(input$date[1], format = "%d.%m.%y")
-    max <- as.Date(input$date[2], format = "%d.%m.%y")
+  # INTERVALL-ANFANG DEFINIEREN
+  min <- reactive({
+    as.Date(input$date[1], format = "%d.%m.%y")
+  })
+  
+  # INTERVALL-ENDE DEFINIEREN
+  max <- reactive({
+    as.Date(input$date[2], format = "%d.%m.%y")
+  })
+  
+  # DATEN NACH INSTITUT FILTERN
+  filtered_data <- reactive({
+    dataset %>% filter(Institut %in% input$institut)
+  })
   
   
+  output$plot1 <- renderPlotly({
+    
+
   # FARBEN FESTLEGEN
-  
-   colors <- c("#019ee3", "#7c7c7c", "#fbeb04", "#1ca42c", "#bd3076", "#f39200", "#e2001a", "#000000")
+   colors <- c("AfD" = "#019ee3", 
+               "Andere" = "#7c7c7c", 
+               "FDP" = "#fbeb04", 
+               "Grüne" = "#1ca42c", 
+               "Linke*" = "#bd3076", 
+               "Piraten" = "#f39200",
+               "SPD" = "#e2001a", 
+               "Union" = "#000000")
    
-   
-   colors_alt <- c("AfD" = "#019ee3", 
-                   "Andere" = "#7c7c7c", 
-                   "FDP" = "#fbeb04", 
-                   "Grüne" = "#1ca42c", 
-                   "Linke*" = "#bd3076", 
-                   "Piraten" = "#f39200",
-                   "SPD" = "#e2001a", 
-                   "Union" = "#000000")
-   
-   # DATEN FILTERN
-   
-   filtered_dataset <- reactive({
-     dplyr::filter(dataset, Institut == input$institut)
-   })
                    
-                   # PLOT 1
-                   
-                   plot1 <- ggplot(data = filtered_dataset()) +
-                     geom_point(aes(x = as.Date(Datum, format = "%d.%m.%y"), 
-                                    y = Prozent, 
-                                    color = as.factor(Partei), 
-                                    shape = as.factor(Institut)), 
-                                stat = "identity", 
-                                size = .8) +
-                     geom_line(aes(x = as.Date(Datum, format = "%d.%m.%y"), 
-                                   y = Prozent, 
-                                   color = as.factor(Partei), 
-                                   linetype = as.factor(Institut))) +
-                     geom_vline(xintercept = as.Date(wahl$Datum, format = "%d.%m.%y"), 
-                                color = "darkgrey", 
-                                linetype = "dashed") +
-                     geom_text(data = wahl, 
-                               aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                                   y = max(na.omit(filtered_dataset()$Prozent))*0.95,
-                                   label = "Ergebnis",
-                                   hjust = -.225,
-                                   vjust = 0,
-                                   angle = 0,
-                                   fontface = 2), 
-                               color = "black",
-                               size = 4) +
-                     geom_text(data = wahl, 
-                               aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                                   y = max(na.omit(filtered_dataset()$Prozent))*0.875,
-                                   label = Name,
-                                   hjust = -.1,
-                                   vjust = 0,
-                                   angle = 0), 
-                               color = "black",
-                               size = 4) +
-                     geom_point(data = wahl_long,
-                                aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                                    y = Prozent,
-                                    color = as.factor(Partei)),
-                                size = 5,
-                                shape = 15) +
-                     # geom_text(data = wahl_long, 
-                     #           aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                     #               y = Prozent,
-                     #               label = Prozent,
-                     #               hjust = -.1,
-                     #               vjust = -1.5,
-                     #               angle = 0), 
-                     #           color = "black",
-                     #           size = 1.5) +
-                     # geom_smooth(method = "loess", se = TRUE, span = 2) +
-                     labs(title = sprintf("Zustimmungswerte der großen politischen Parteien seit %s", format(min, "%d.%m.%y")),
-                          subtitle = "Stand: 17. Juli 2021",
-                          color = "Parteien",
-                          linetype = "Institute",
-                          shape = "Institute",
-                          x = "Datum",
-                          caption = "github.com/dominiklawetzky/sonntagsfrage") +
-                     scale_color_manual(name = "Parteien", values = colors_alt) +
-                     #theme_fivethirtyeight() +
-                     theme_light() +
-                     theme(axis.text.x=element_text(size=rel(.75), 
-                                                    angle=90, 
-                                                    margin = margin(b = 12)),
-                           plot.title = element_text(size = 18, 
-                                                     face = "bold"),
-                           plot.margin = unit(c(1,1,1,1), "cm"),
-                           legend.position="bottom") +
-                     scale_x_date(breaks = "6 month", 
-                                  limits = c(min, max))
-                   
-                   
-                   
-                   plot1_gl <- ggplot(data = filtered_dataset()) +
-                     geom_vline(xintercept = as.Date(wahl$Datum, format = "%d.%m.%y"), 
-                                color = "darkgrey", 
-                                linetype = "dashed") +
-                     geom_text(data = wahl, 
-                               aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                                   y = max(na.omit(filtered_dataset()$Prozent))*0.95,
-                                   label = "Ergebnis",
-                                   hjust = -.225,
-                                   vjust = 0,
-                                   angle = 0,
-                                   fontface = 2), 
-                               color = "black",
-                               size = 4) +
-                     geom_text(data = wahl, 
-                               aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                                   y = max(na.omit(filtered_dataset()$Prozent))*0.875,
-                                   label = Name,
-                                   hjust = -.1,
-                                   vjust = 0,
-                                   angle = 0), 
-                               color = "black",
-                               size = 4) +
-                     geom_point(data = wahl_long,
-                                aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                                    y = Prozent,
-                                    color = as.factor(Partei)),
-                                size = 5,
-                                shape = 15) +
-                     # geom_text(data = wahl_long, 
-                     #           aes(x = as.Date(Datum, format = "%d.%m.%y"),
-                     #               y = Prozent,
-                     #               label = Prozent,
-                     #               hjust = -.1,
-                     #               vjust = -1.5,
-                     #               angle = 0), 
-                     #           color = "black",
-                     #           size = 1.5) +
-                     geom_smooth(aes(x = as.Date(Datum, format = "%d.%m.%y"), 
-                                     y = Prozent, 
-                                     color = as.factor(Partei), 
-                                     shape = as.factor(Institut)),
-                                 method = "loess", se = TRUE, span = input$f) +
-                     labs(title = sprintf("Zustimmungswerte der großen politischen Parteien seit %s", format(min, "%d.%m.%y")),
-                          subtitle = "Stand: 17. Juli 2021",
-                          color = "Parteien",
-                          linetype = "Institute",
-                          shape = "Institute",
-                          x = "Datum",
-                          caption = "github.com/dominiklawetzky/sonntagsfrage") +
-                     scale_color_manual(name = "Parteien", values = colors_alt) +
-                     #theme_fivethirtyeight() +
-                     theme_light() +
-                     theme(axis.text.x=element_text(size=rel(.75), 
-                                                    angle=90, 
-                                                    margin = margin(b = 12)),
-                           plot.title = element_text(size = 18, 
-                                                     face = "bold"),
-                           plot.margin = unit(c(1,1,1,1), "cm"),
-                           legend.position="bottom") +
-                     scale_x_date(breaks = "6 month", 
-                                  limits = c(min, max))
-                   
-                   ggsave("plot.pdf", plot1)
-                   
-                   if(input$smoother == 1){print(plot1_gl)}
-                   else {print(plot1)}
+   plot1 <- filtered_data() %>% filter(between(Datum, as.Date(min()), as.Date(max()))) %>%
+                 plot_ly(x = ~Datum, y = ~Prozent) %>%
+                   add_lines(color = ~Partei, 
+                             colors = colors,
+                             linetype = ~as.factor(Institut)) %>%
+                   layout(title = sprintf("Zustimmungswerte der großen politischen Parteien seit %s", format(min(), "%d.%m.%y")),
+                          # legend = list(orientation = "h",
+                          #               xanchor = "center",
+                          #               x = .5,
+                          #               y = -.45),
+                          margin = c(1,1,1,1))
                    
     
   })
   
-  
-
-  output$plot1_download <- downloadHandler(
-    filename = function() {
-      "plot.pdf"
-    },
-    content = function(file) {
-      file.copy("plot.pdf", file, overwrite=TRUE)
-    }
-  )
   
 
 }
