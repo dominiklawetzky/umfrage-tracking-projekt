26cc706bc217c2d5fa95af44a80f27a14329318b
Dominik Lawetzky <dominik@lawetzky.de>
2021-07-26 09:22:35 +0200

    Deploybare Dateien in separaten Ordner

---
+++ convert.js
@@ -0,0 +1,51 @@
+
+var showdown  = require('showdown');
+var fs = require('fs');
+let filename = "README.md"
+let pageTitle = process.argv[2] || ""
+
+fs.readFile(__dirname + '/style.css', function (err, styleData) {
+  fs.readFile(process.cwd() + '/' + filename, function (err, data) {
+    if (err) {
+      throw err;
+    }
+    let text = data.toString();
+
+    converter = new showdown.Converter({
+      ghCompatibleHeaderId: true,
+      simpleLineBreaks: true,
+      ghMentions: true,
+      tables: true
+    });
+
+    let preContent = `
+    <html>
+      <head>
+        <title>` + pageTitle + `</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
+      <body>
+        <div id='content'>
+    `
+
+    let postContent = `
+        </div>
+        <style type='text/css'>` + styleData + `</style>
+      </body>
+    </html>`;
+
+    html = preContent + converter.makeHtml(text) + postContent
+
+    converter.setFlavor('github');
+    console.log(html);
+
+    let filePath = process.cwd() + "/README.html";
+    fs.writeFile(filePath, html, { flag: "wx" }, function(err) {
+      if (err) {
+        console.log("File '" + filePath + "' already exists. Aborted!");
+      } else {
+        console.log("Done, saved to " + filePath);
+      }
+    });
+  });
+});
